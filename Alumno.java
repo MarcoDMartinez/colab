571ee1aea7c241f9ac8883c03d13ecce1d766ebb
BrianGalvezchess <20253rd056@utez.edu.mx>
2026-03-05 15:39:06 -0600

    Agregue datos al archivo alumno

--- Alumno.java
+++ Alumno.java
@@ -1,3 +1,13 @@
 public class Alumno {
+    protected String nombre;
+    protected String matricula;
+    protected int calificacion1;
+    protected int calificacion2;
+    protected int calificacion3;
+
     
-}
+
+
+
+    }
+
