262d364fb7addb90a3599288696598874e8648df
BrianGalvezchess <20253rd056@utez.edu.mx>
2026-03-05 15:49:24 -0600

    Hize el metodo de calcularPromedio para el Alumno

--- Alumno.java
+++ Alumno.java
@@ -1,15 +1,24 @@
 public class Alumno {
     String nombre;
     protected String matricula;
-    protected int calificacion1;
-    protected int calificacion2;
-    protected int calificacion3;
+    protected double calificacion1;
+    protected double  calificacion2;
+    protected double  calificacion3;
 
     public Alumno(String nombre){
         this.nombre = nombre;
     }
 
+    //metodo del alumno
+    //mostrar calcular promedio
+    public double calcularPromedio(){
+        return (calificacion1 + calificacion2 + calificacion3) / 3;
+    }
 
+    public void mostrarAlumno(){
+        System.out.println("---Reporte del alumno---");
+        System.out.println("");
+    }
 
     }
 
