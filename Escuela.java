1ccfabe2b6cf8387b5e7696519dd46f7d671b7de
MarcoDMartinez <20253rd035@utez.edu.mx>
2026-03-05 16:31:06 -0600

    Termine el archivo escuela. junto a sus overrides, y tambien hice 4 objetos en Main

--- Escuela.java
+++ Escuela.java
@@ -1,5 +1,19 @@
 public class Escuela extends Alumno{
 
-    
+        public Escuela(String nombre, String matricula){
+
+            super(nombre, matricula);
+
+            this.nombre = nombre;
+
+    }
+
+    @Override
+        public void mostrarAlumno(){
+        System.out.println("---Reporte del alumno---");
+        System.out.println("Nombre : " + nombre);
+        System.out.println("matricula: " + matricula);
+        System.out.println("Registro oficial escolar.");
+    }
     
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-            Alumno alumno1 = new Alumno("Pedro");
+            Alumno alumno1 = new Alumno("Pedro", "20253rd035");
 
             PedirDatos pd1 = new PedirDatos();
             int calificacion1 = pd1.pedirEntero("Escribe la primera calificación");
@@ -12,6 +12,21 @@ public class Main {
             alumno1.setpromedio((calificacion1 + calificacion2 + calificacion3) / 3);
 
             alumno1.calcularPromedio();
+
+            alumno1.mostrarAlumno();
+
+            Escuela alumno2 = new Escuela("Pedro", "20253rd035");
+
+            PedirDatos pd2 = new PedirDatos();
+             calificacion1 = pd2.pedirEntero("Escribe la primera calificación");
+             calificacion2 = pd2.pedirEntero("Escribe la segunda calificación");
+             calificacion3 = pd2.pedirEntero("Escribe la tercera calificación");
+
+            alumno2.setpromedio((calificacion1 + calificacion2 + calificacion3) / 3);
+
+            alumno2.calcularPromedio();
+
+            alumno2.mostrarAlumno();
 
 
     }
